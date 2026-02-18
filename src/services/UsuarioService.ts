a27aafeb642ae2c8363282b280034c0cf615addb
hernan3154 <herny3154@gmail.com>
2026-02-18 01:33:10 -0300

    cambio en usuarioService

--- src/services/UsuarioService.ts
+++ src/services/UsuarioService.ts
@@ -1,22 +1,20 @@
 // src/services/UsuarioService.ts
 import { apiAuth } from '../api/InstanceAxios';
 
-// Agregamos 'usuarios/' antes de login
 export const loginRequest = async (email: string, pass: string) => {
-    const { data } = await apiAuth.post('usuarios/login/', { email, password: pass });
+    // Forzamos que esta petición use credenciales
+    const { data } = await apiAuth.post('usuarios/login/', 
+        { email, password: pass }
+    );
     return data;
 };
 
 export const logoutRequest = async () => {
-    // Agregamos 'usuarios/' antes de logout
-    return await apiAuth.post('usuarios/logout/', { 
-        //withCredentials: true 
-    });
+    return await apiAuth.post('usuarios/logout/');
 };
 
 export const checkSessionRequest = async () => {
-    // Quitamos la barra inicial si tu InstanceAxios ya tiene '/api/' 
-    // y nos aseguramos de que sea 'usuarios/me/'
+    // Esta es la que nos dirá la verdad: ¿Django nos reconoce?
     const { data } = await apiAuth.get('usuarios/me/'); 
     return data;
 };
